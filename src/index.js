7209bac99a3c140cb5bee496cec7c35318ea349e
ayushkumar <ayushkumarlfs2@gmail.com>
2023-06-11 11:47:10 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter,Routes, Route} from 'react-router-dom';
+import {  HashRouter,Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Meditation from './pages/Medcorner';
 import Events from './pages/events';
 
 export default function RRR() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="meditation" element={<Meditation />} />
             <Route path="events" element={<Events />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
