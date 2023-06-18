d9239d04ad6080c2eebc9ad3e4b965eb4f05a10f
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2023-06-18 19:01:31 +0530

    Add files via upload

--- src/pages/Medcorner.js
+++ src/pages/Medcorner.js
@@ -9,7 +9,7 @@ const medcorner = () =>{
             <Navbar/>
                 <div className="medcorner-container">
                     <div className="max-width">
-                    <h1>Tranquil treat for <br/> <span>Mindfulness</span>.</h1>
+                    <h1>Tranquil treat for <br/> <span>Mindfulness.</span></h1>
                     <p> Step into our Meditation Corner and let the melodies embrace <br/>you on your path to <span>tranquility and well-being.</span></p>
                     </div>
                 </div>
